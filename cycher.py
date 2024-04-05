e664b0828d01469d65ae48f255f20ed13877d829
KJhenelle <Jhenelle.walters.5@gmail.com>
2024-04-04 21:19:25 -0400

    Update cycher.py

--- cycher.py
+++ cycher.py
@@ -1,9 +1,10 @@
 import sys
 def complex_cipher():
-    strr=str(input('Enter the message you want encoded: '))
+    strr=str(input('Enter the message you want encoded (numbers will not be encrypted): '))
     key= int(input('enter your encrytion key: '))
     cypt=[]
     final=''
+    separ =input ('what would you like for non letters to be represented by: ')
     for char in strr:
         if ord(char)<65 or (ord(char)>90 and ord(char)<96):
             try: 
@@ -11,7 +12,7 @@ def complex_cipher():
                 cypt.append(char)
                 
             except: 
-                cypt.append('/')
+                cypt.append(separ)
         
         elif char.upper() == char:
 
@@ -38,14 +39,31 @@ def complex_cipher():
 
 # for nu in range(97,123):
 # #     print(chr(nu))
-# def simple_cipher():
-#     strrr=str(input('Enter the message you want encoded: '))
-#     keyr= int(input('enter your encrytion key: '))
-#     cyptr=[]
-#     finalr=''
-#     strrr.upper()
-#     for char in strrr:
-#         cyptr.append(char)
+def simple_cipher():
+    strrr=str(input('Enter the message you want encoded: '))
+    keyr= int(input('enter your encrytion key: '))
+    cyptr=[]
+    finalr=''
+    strrr=strrr.upper()
+    five=0
+    for char in strrr:
+        
+        if (ord(char)+keyr)>90:
 
+            cyptr.append(chr((ord(char))+keyr-26))
+        elif (ord(char))>90 or (ord(char))<65:
+            continue
+        else:
 
-print(complex_cipher())
+            cyptr.append(chr((ord(char))+keyr))
+    for x in cyptr:
+        if five==5:
+                finalr+= ' '
+                finalr+=x
+                five=0
+        else:
+                finalr+=x
+                five+=1
+    return finalr
+
+print(simple_cipher())
